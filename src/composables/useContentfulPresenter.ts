3dc5cd64c7827836b0a2ba6a148eb84d9d872b37
Mostafa Rahmati <45990659+mostafa7904@users.noreply.github.com>
2022-09-10 12:09:12 +0430

    chore: rename undescriptive names
    Rename function parameters in useContentfulPresenter

--- src/composables/useContentfulPresenter.ts
+++ src/composables/useContentfulPresenter.ts
@@ -1,13 +1,13 @@
 import type { EntryCollection, Asset } from 'contentful'
 
 export function useContentfulPresenter<T>(
-  _entries: EntryCollection<T>,
+  entryCollection: EntryCollection<T>,
   assetFieldName: string = 'image',
 ): T[][] {
-  const entries = _entries.items.map((entry) => {
+  const modifiedEntries = entryCollection.items.map((entry) => {
     const fields = entry.fields as any
     fields[assetFieldName] = (fields[assetFieldName] as Asset).fields.file.url
     return fields
   })
-  return [entries]
+  return [modifiedEntries]
 }
